ef61d9f2e7d90adbcd694d6a8928c2134b179ee5
Ryan Guerrettaz <rguerrettaz@yahoo.com>
2015-03-19 19:04:02 -0700

    remove timestamps on users table

--- schema.js
+++ schema.js
@@ -2,9 +2,7 @@ var Schema = {
   users: {
     id:         {type: 'increments', nullable: false, primary: true},
     email:      {type: 'string', maxlength: 254, nullable: false, unique: true},
-    name:       {type: 'string', maxlength: 150, nullable: false},
-    created_at: {type: 'dateTime', nullable: false},
-    updated_at: {type: 'dateTime', nullable: true}
+    name:       {type: 'string', maxlength: 150, nullable: false}
   },
   categories: {
     id:   {type: 'increments', nullable: false, primary: true},
